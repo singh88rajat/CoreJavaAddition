dd164f226ca74c20e6191c884e357040b4dd4df0
Mukesh Kumar <singh88rajat@gmail.com>
2016-12-17 22:02:12 +0530

    CoreJavaAddition

---
+++ src/MySecondJavaAssignment.java
@@ -0,0 +1,11 @@
+
+public class MySecondJavaAssignment {
+	 public static void main(String args[]) {
+	      int x=10;
+	      int y=20;
+	      int sum=x-(-y);
+	       System.out.println(" Sum of X add Y is :"+sum);
+	       
+	    }  
+	
+}
